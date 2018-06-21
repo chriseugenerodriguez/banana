5fbdbbb632b41db6f4201211a098b018d40aad70
Chris Rodriguez <info@premiumwd.com>
2018-06-20 22:30:31 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IntlService } from '@progress/kendo-angular-intl';
+
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public range = { start: null, end: null };
+  total: number;
+  public start: any;
+  public end: any;
+  public login: FormGroup;
+
+  msecPerMinute = 1000 * 60;  
+  msecPerHour = this.msecPerMinute * 60;  
+  msecPerDay = this.msecPerHour * 24; 
+
+  constructor(private intl: IntlService, private fb: FormBuilder) {
+
+    this.form();
+  }
+
+  ngOnInit() {
+
+    this.login.valueChanges.subscribe(
+      data => {
+        this.range.start = data['start'];
+        this.range.end = data['end'];
+
+        if(this.range.start) {
+          this.start = this.range.start
+        }
+        if(this.range.end) {
+          this.end = this.range.end
+        }
+        
+        var interval = this.end - this.start;  
+        var days = Math.floor(interval / this.msecPerDay );  
+
+        let amount = (.05) * Math.floor(days/7);
+        this.total = Math.ceil(amount * 100) / 100;
+      }
+    );
+  }
+
+	private form() {
+		return this.login = this.fb.group({
+			start: ['', Validators.required],
+			end: ['', Validators.required],
+		});
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { IntlModule } from '@progress/kendo-angular-intl';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IntlModule, 
+    DateInputsModule, 
+    LabelModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
